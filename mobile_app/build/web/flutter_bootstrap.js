7ee1c15b78988b977b289682dfd28edb40bf6d30
Matthew Gift Luis <matt@Matthews-MacBook-Pro.local>
2026-01-07 13:49:20 +0200

    build(release): web and apk update

--- mobile_app/build/web/flutter_bootstrap.js
+++ mobile_app/build/web/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"a5cb96369ef86c7e85abf5d662a1ca5d897750
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1289241220"
+    serviceWorkerVersion: "2791421540"
   }
 });
--- mobile_app/build/web/flutter_service_worker.js
+++ mobile_app/build/web/flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "59869c0fbe4beb416e9ab30c6e716010",
+const RESOURCES = {"flutter_bootstrap.js": "198bce8ce40418d9dbc2529abe6d74d0",
 "version.json": "a7e55a991cc69f60ca0a7cbd4169ae40",
 "index.html": "8fc5f1f137a62b868f572eaf6a050b99",
 "/": "8fc5f1f137a62b868f572eaf6a050b99",
