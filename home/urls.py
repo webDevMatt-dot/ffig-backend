6466fb7a4a20182598bb03a8f54f799c3a4bb6fe
Matthew Gift Luis <matt@Matthews-MacBook-Pro.local>
2026-01-07 18:21:10 +0200

    build(release): web and apk update

--- home/urls.py
+++ home/urls.py
@@ -6,10 +6,10 @@ from .views import (
 )
 
 router = DefaultRouter()
-router.register(r'hero', HeroItemViewSet)
-router.register(r'founder', FounderProfileViewSet)
-router.register(r'alerts', FlashAlertViewSet)
-router.register(r'ticker', NewsTickerItemViewSet)
+router.register(r'hero', HeroItemViewSet, basename='heroitem')
+router.register(r'founder', FounderProfileViewSet, basename='founderprofile')
+router.register(r'alerts', FlashAlertViewSet, basename='flashalert')
+router.register(r'ticker', NewsTickerItemViewSet, basename='newstickeritem')
 
 urlpatterns = [
     path('', include(router.urls)),
