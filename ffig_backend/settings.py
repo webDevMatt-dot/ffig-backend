f19fbc5e76db6187d1998f8480db16deee8c7b6c
Matthew Gift Luis <matt@Matthews-MacBook-Pro.local>
2026-01-10 04:17:51 +0200

    build(release): web and apk update

--- ffig_backend/settings.py
+++ ffig_backend/settings.py
@@ -95,6 +95,7 @@ WSGI_APPLICATION = 'ffig_backend.wsgi.application'
 # DATA BASE CONFIGURATION
 # If there is a DATABASE_URL environment variable (Render), use it.
 # Otherwise, use your local sqlite file.
+DATABASES = {
     'default': dj_database_url.config(
         default='sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3'),
         conn_max_age=600
